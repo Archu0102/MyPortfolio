b0d6e81833c29d9b763370fb631ed4d9ae177fe0
Archu0102 <archush0102@gmail.com>
2023-01-22 22:20:48 +0530

    change in component file

--- src/component/AboutContent.js
+++ src/component/AboutContent.js
@@ -1,8 +1,31 @@
+import './AboutContentStyle.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ReactImg from '../assets/react3.png'
+import CssImg from '../assets/css3.jpg'
 
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
+    <div className='about'>
+        <div className='left'>
+            <h1>I'm Archana Kumari</h1>
+            <p>I'm react front-end developer. i have create responsive websites</p>
+            <Link to="/contact">
+                <button className='btn'>Contact</button>
+            </Link>
+        </div>
+
+        <div className='right'>
+            <div className='img-container'>
+                <div className='img-stack top'>
+                    <img src={ReactImg} alt="React image"/>
+                </div>
+                <div className='img-stack top'>
+                    <img src={CssImg} alt="CSS image"/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../component/Navbar';
 import Footer from '../component/Footer';
 import Heroimage2 from '../component/Heroimage2';
+import AboutContent from '../component/AboutContent';
 
 const Aboutpage = () =>{
     return(
         <div>
            <Navbar/>
            <Heroimage2 heading="ABOUT" text="I'm Friendly Front-end developer" />
+           <AboutContent/>
            <Footer/>
         </div>
     )
