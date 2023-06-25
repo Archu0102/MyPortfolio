09f3922c9a22a3ad949ff7c61d02bd59cd42b1ef
Archu0102 <archush0102@gmail.com>
2023-06-25 17:20:22 +0530

    change in component

--- src/component/WorkcardData.js
+++ src/component/WorkcardData.js
@@ -1,53 +1,54 @@
 import pro1 from '../assets/dice.png'
 import pro2 from '../assets/love cal.jpg'
 import pro3 from '../assets/facebook img.jpg'
-import pro4 from '../assets/laptop.jpg'
-import pro5 from '../assets/laptop.jpg'
-import pro6 from '../assets/laptop.jpg'
-import pro7 from '../assets/laptop.jpg'
+//import pro4 from '../assets/laptop.jpg'
+import pro4 from '../assets/travel.png'
+import pro5 from '../assets/navbar.png'
+import pro6 from '../assets/simplelogin.png'
+import pro7 from '../assets/register.png'
 
 const ProjectCardData = [
     {
-        imgsrc: pro1,
-        title: "Dice Game",
-        text: "This is the dice-game which i made, here i used HTML and CSS.it's very easy and simple to play.You can play it with your friends and family members, this game realy amazing.",
-        view: "https://www.youtube.com/watch?v=B17NC5DD-dA"
-    },
-    {
-        imgsrc: pro2,
-        title: "Love Calculator",
-        text: "Love Calculator is a type of calculator which calculat Love between two person, For creating this calculator i used HTML, CSS and JavaScript. This game realy amazing.",
-        view: "https://www.youtube.com/watch?v=ns6kKTf33tU"
+        imgsrc: pro4,
+        title: "Travel Project",
+        text: "This project is Travel project here custommer can contact us for vising new place. It build using react",
+        view: "https://github.com/Archu0102/Travel_Project"
     },
     {
         imgsrc: pro3,
         title: "FaceBook Page",
         text: "This project is just like a facebook page, in this project i used HTML and CSS. You can also enjoy it.",
-        view: "https://www.youtube.com/watch?v=NljIHlZRTTE"
-    },
-    {
-        imgsrc: pro4,
-        title: "Travel Project",
-        text: "This project is Travel based project and it is very usfull and it provide very easy travel to the custommer",
-        view: ""
+        view: "https://github.com/Archu0102/Assignment"
     },
     {
         imgsrc: pro5,
         title: "Simple Navbar",
-        text: "This is simple navbar",
-        view: ""
+        text: "This is simple navbar, here I used react ",
+        view: "https://github.com/Archu0102/React_Navbar"
     },
     {
-        imgsrc: pro6,
-        title: "Login Register Page",
-        text: "This is simple Login Register Page",
-        view: ""
+        imgsrc: pro1,
+        title: "Dice Game",
+        text: "This is the dice-game which i made, here i used HTML and CSS.it's very easy and simple to play.You can play it with your friends and family members, this game realy amazing.",
+        view: "https://github.com/Archu0102/dicee"
     },
     {
         imgsrc: pro7,
-        title: "SuperShop",
-        text: "This is simple Login Register Page",
-        view: ""
+        title: "Register Page",
+        text: "This is Login Register Page using React, node, express and mongoDB, here all the data strored in the database.",
+        view: "https://github.com/Archu0102/loginandRegisterPage"
+    },
+    {
+        imgsrc: pro2,
+        title: "Love Calculator",
+        text: "Love Calculator is a type of calculator which calculat Love between two person, For creating this calculator i used HTML, CSS and JavaScript. This game realy amazing.",
+        view: "https://github.com/Archu0102/lovecalculator"
+    },
+    {
+        imgsrc: pro6,
+        title: "Login/Logout Page",
+        text: "This is simple Login Register Page using react only frontend. ",
+        view: "https://github.com/Archu0102/Login-Logout-page"
     }
 ];
 
